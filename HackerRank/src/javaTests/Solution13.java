dd9b21c24516d51eeab5d888dce0f4ea92460e02
pedro.favari <pedro.favari@NB-VERITY000234.verity.local>
2017-10-11 13:55:50 -0300

    -Solution13

--- HackerRank/src/javaTests/Solution13.java
+++ HackerRank/src/javaTests/Solution13.java
@@ -4,21 +4,15 @@ import java.util.*;
 import java.text.*;
 
 public class Solution13 {
-    
-    public static void main(String[] args) {
-    	java.util.Currency usd = java.util.Currency.getInstance("USD");
-    	NumberFormat format = NumberFormat.getCurrencyInstance(Locale.US);
-        Scanner scanner = new Scanner(System.in);
-        double payment = scanner.nextDouble();
-        scanner.close();
-        
-       format.setCurrency(usd);
-        
-        // Write your code here.
-        
-        System.out.println("US: " + format.format(payment));
-//        System.out.println("India: " + india);
-//        System.out.println("China: " + china);
-//        System.out.println("France: " + france);
-    }
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double payment = scanner.nextDouble();
+		scanner.close();
+
+		System.out.println("US: " + NumberFormat.getCurrencyInstance(new Locale("en", "US")).format(payment));
+		System.out.println("India: " + NumberFormat.getCurrencyInstance(new Locale("en", "IN")).format(payment));
+		System.out.println("China: " + NumberFormat.getCurrencyInstance(new Locale("zh", "CN")).format(payment));
+		System.out.println("France: " + NumberFormat.getCurrencyInstance(new Locale("fr", "FR")).format(payment));
+	}
 }
