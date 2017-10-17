cf6887bd6f14ff8c7930284c81a9a013e15ceb7a
pedro.favari <pedro.favari@NB-VERITY000234.verity.local>
2017-10-17 09:48:50 -0200

    Java Regex

--- HackerRank/src/javaTests/Strings/Solution8.java
+++ HackerRank/src/javaTests/Strings/Solution8.java
@@ -17,8 +17,8 @@ class MyRegex {
 	String pattern;
 
 	MyRegex() {
-		String zeroTo255 = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
-		pattern = zeroTo255 + "." + zeroTo255 + "." + zeroTo255 + "." + zeroTo255;
+		String ipPart = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
+		pattern = ipPart + "." + ipPart + "." + ipPart + "." + ipPart;
 
 	}
 }
