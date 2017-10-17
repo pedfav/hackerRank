aaa2c7ace72726891e6a0a92e9b73f0697ac52c0
pedro.favari <pedro.favari@NB-VERITY000234.verity.local>
2017-10-17 09:47:46 -0200

    Java Regex

---
+++ HackerRank/src/javaTests/Strings/Solution8.java
@@ -0,0 +1,24 @@
+package javaTests.Strings;
+
+import java.util.Scanner;
+
+class Solution8 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			String IP = in.next();
+			System.out.println(IP.matches(new MyRegex().pattern));
+		}
+	}
+}
+
+class MyRegex {
+	String pattern;
+
+	MyRegex() {
+		String zeroTo255 = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
+		pattern = zeroTo255 + "." + zeroTo255 + "." + zeroTo255 + "." + zeroTo255;
+
+	}
+}
