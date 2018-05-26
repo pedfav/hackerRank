253d45342fc351ce6724ef7f032f2e028c59eba4
pedfav <pedfav@gmail.com>
2018-05-26 13:26:57 -0300

    change directory

--- src/javaTests/introduction/Solution1.java
+++ src/javaTests/introduction/Solution1.java
@@ -1,7 +1,6 @@
 package javaTests.introduction;
 
 public class Solution1 {
-
     public static void main(String[] args) {
         /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
         System.out.println("Hello, World.");
