3398c0105fba883c766a39eb0dee34f1387a1518
pedrofavari <pedro.favari@netshoes.com>
2018-06-07 10:01:36 -0300

    "30 days of code: 3 - change in strings"

--- src/Strings/Solution1/JavaStringsIntroduction.java
+++ src/Strings/Solution1/JavaStringsIntroduction.java
@@ -9,7 +9,8 @@ public class JavaStringsIntroduction {
         String A=sc.next();
         String B=sc.next();
         
-         System.out.println(A.length() + B.length());
+        System.out.println(A.length() + B.length());
+
         if(A.compareTo(B) < 0){
         	System.out.println("No");
         } else if (A.compareTo(B) == 0){
@@ -17,8 +18,7 @@ public class JavaStringsIntroduction {
         } else {
             System.out.println("Yes");
         }
-        System.out.println(A.substring(0,1).toUpperCase() + A.substring(1, A.length()) 
-        		+ " " + B.substring(0,1).toUpperCase() + B.substring(1, B.length()));
-        
+
+        System.out.println(A.substring(0,1).toUpperCase() + A.substring(1, A.length()) + " " + B.substring(0,1).toUpperCase() + B.substring(1, B.length()));
     }
 }
--- src/Strings/Solution10/UsernameValidator.java
+++
@@ -1,8 +0,0 @@
-package Strings.Solution10;
-
-class UsernameValidator {
-    /*
-     * Write regular expression here.
-     */
-    public static final String regularExpression = "/^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]*$/";
-}
